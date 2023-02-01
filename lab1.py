8cb7d7465e3551a3211b28e6bb81184ba300bf60
Grace Anspach <123026604+Ganspach01@users.noreply.github.com>
2023-02-01 16:11:53 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,73 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#the author's name is: Grace Anspach
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("Helo\nare\nyou?")
+Helo
+are
+you?
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("Today"+ "is"+ "Wednesday")
+TodayisWednesday
+#explain1: it gives you what is in the quotations
+#explain2: it makes a new line
+#explain3: they are combined together without spaces
+print("123\n45\n6")
+123
+45
+6
+print("a\nb\nc")
+a
+b
+c
+print("Helo"+"World")
+HeloWorld
+print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+#explain1: yes it does
+#explain2: / divides and // is division without the remainder
+#explain3: % finds the remainder
+#explain4: ** is an exponent
+(4+3)//2
+3
+6%2
+0
+1+2*3**4
+163
+print("2+2=", 2+2)
+2+2= 4
+print("2+2=",2+2)
+2+2= 4
+print("50 pounds is ", 50*0.453492, "Kilograms")
+50 pounds is  22.6746 Kilograms
+print("50 pounds is ",50*0.453492,"Kilograms")
+50 pounds is  22.6746 Kilograms
